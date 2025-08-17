e48bcab8649849057e14d20a785f1a67fe7640cc
Alexander Grachev <sashagrachev2005@gmail.com>
2025-08-17 11:53:55 +0300

    Add licenses and copyrights

--- cmd_to_odom_package/cmd_to_odom_package/cmd_to_odom_node.py
+++ cmd_to_odom_package/cmd_to_odom_package/cmd_to_odom_node.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# Copyright (c) 2025 Alice Zenina and Alexander Grachev RTU MIREA (Russia)
+# SPDX-License-Identifier: MIT
+# Details in the LICENSE file in the root of the package.
+
 import rclpy
 from rclpy.node import Node
 from rclpy.parameter import Parameter
